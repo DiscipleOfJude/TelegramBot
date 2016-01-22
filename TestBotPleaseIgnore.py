552bcf128f2a1e439daff13443d20a6afeaf7aab
raeltik <sirch0883@gmail.com>
2016-01-22 15:00:21 -0500

    fixing grammar

--- TestBotPleaseIgnore.py
+++ TestBotPleaseIgnore.py
@@ -19,11 +19,11 @@ def help(bot, update):
 def verifyme(bot, update):
     code = random.randrange(10000,99999)
     bot.sendMessage(update.message.chat_id, text="So you want to verify? Great! That's how you get into our main" +
-                    "room!\n What we need from you is a picture, but not just any picture. It needs to be of you," +
-                    "your partner(s), and show you holding something with the following things visible.\n 1) Your "+
-                    "usernames.\n 2) The current date\n 3) The following code {}".format(code) +
-                    "Make sure you picture contains those three things, there are no exceptions." +
-                    "\n\n  The Admins of this room will check your photo, and may ask you further questions.\n " +
+                    "room! What we need from you is a picture, but not just any picture. It needs to be of you," +
+                    "your partner(s), and  has to show you holding something with the following things visible.\n\n"+
+                    "1) Your usernames.\n 2) The current date\n 3) The following code {} ".format(code) +
+                    "\nMake sure you picture contains those three things, there are no exceptions." +
+                    "\n\nThe Admins of this room will check your photo, and may ask you further questions.\n" +
                     "The Admins will never PM you, if you are PM'ed from anyone in this holding room they are not" +
                     " an admin, nor are they verified. Do not trust them." +
                     "\n\n\n WE RESERVE THE RIGHT TO REFUSE ENTERANCE FROM OUR MAIN ROOM")
