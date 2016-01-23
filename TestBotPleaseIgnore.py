add93d167ad54d5b60f35ccd38d57dc098fe46bb
raeltik <sirch0883@gmail.com>
2016-01-22 19:56:00 -0500

    asdfa

--- TestBotPleaseIgnore.py
+++ TestBotPleaseIgnore.py
@@ -32,6 +32,8 @@ def verifyme(bot, update):
 
 
 def verifypic(bot, update):
+   file_id = bot.getUpdates()[-1].photo.file_id
+   print(file_id)
    print(update.message.message_id)
    last_id = update.message.message_id - 1
    updates = telegram.Message(last_id,update.message.from_user.id,update.message.date,update.message.chat_id)
